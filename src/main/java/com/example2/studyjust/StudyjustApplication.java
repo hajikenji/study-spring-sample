06d85e00cb6c710297443e27872f13e4f82ee7a5
hajikenji <mademoiselle3824@gmail.com>
2022-05-25 10:22:50 +0900

    初回コミット

---
+++ src/main/java/com/example2/studyjust/StudyjustApplication.java
@@ -0,0 +1,13 @@
+package com.example2.studyjust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyjustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyjustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example2/studyjust/StudyjustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2.studyjust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyjustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
